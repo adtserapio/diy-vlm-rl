af96f22f3ebac80816533e3ba44ea34830c76a77
Adrian Serapio <adtserapio@gmail.com>
2025-03-29 11:25:45 +0900

    Update

--- main.py
+++ main.py
@@ -1,58 +1,99 @@
-from grpo_trainer import MinimalGRPOTrainer
+import os
+from datasets import load_dataset
+from grpo import QwenGRPOTrainer, accuracy_reward, format_reward
 from trl.trainer.grpo_config import GRPOConfig
 import torch
 import regex as re
+import json
+from transformers.integrations.deepspeed import is_deepspeed_zero3_enabled
+from trl import GRPOConfig, GRPOTrainer, ModelConfig, ScriptArguments, TrlParser, get_peft_config
+from dataclasses import dataclass, field
+from typing import Optional
 
-def format_reward(prompt, completion, **kwargs):
-    return 1.0 if ("<think>" in completion and "</think>" in completion and "<answer>" in completion and "</answer>" in completion) else -1.0 
+os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1"
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-if __name__ == "__main__":
-    config = GRPOConfig()
-    config.num_iterations = 1
-    config.num_generations = 2
-    config.max_steps = 1000
-    config.per_device_train_batch_size = 2
-    config.gradient_accumulation_steps = 1
-    config.gradient_checkpointing=True
-    config.report_to = "wandb"
-    config.run_name = "smolvlm2_format_reward"
-    config.logging_steps= 1
+@dataclass
+class GRPOScriptArguments(ScriptArguments):
+    """
+    Script arguments for the GRPO training script.
 
-    model_id = "HuggingFaceTB/SmolVLM2-2.2B-Instruct"  
-    original_train_dataset = [
-        {
-            "prompt": "Generate a radiology report for this Chest X-ray image. The reasoning process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e. <think> reasoning process here </think><answer> radiology report </answer>",
-            "image_path": "/mnt/sohn2022/MIMIC-CXR-JPG/physionet.org/files/mimic-cxr-jpg/2.0.0/files/p10/p10000032/s50414267/02aa804e-bde0afdd-112c0b34-7bc16630-4e384014.jpg"
-        },
-        {
-            "prompt": "Generate a radiology report for this Chest X-ray image. The reasoning process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e. <think> reasoning process here </think><answer> radiology report </answer>",
-            "image_path": "/mnt/sohn2022/MIMIC-CXR-JPG/physionet.org/files/mimic-cxr-jpg/2.0.0/files/p10/p10000032/s56699142/ea030e7a-2e3b1346-bc518786-7a8fd698-f673b44c.jpg"
-        }
-    ] * 32
+    Args:
+        reward_funcs (`list[str]`):
+            List of reward functions. Possible values: 'accuracy', 'format'.
+    """
 
+    reward_funcs: list[str] = field(
+        default_factory=lambda: ["accuracy", "format"],
+        metadata={"help": "List of reward functions. Possible values: 'accuracy', 'format'"},
+    )
+    max_pixels: Optional[int] = field(
+        default=12845056,
+        metadata={"help": "Maximum number of pixels for the image"},
+    )
+    min_pixels: Optional[int] = field(
+        default=3136,
+        metadata={"help": "Minimum number of pixels for the image"},
+    )
+
+
+SYSTEM_PROMPT = (
+    "A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant "
+    "first thinks about the reasoning process in the mind and then provides the user with the answer. The reasoning "
+    "process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e., "
+    "<think> reasoning process here </think><answer> answer here </answer>"
+)
+
+QUESTION_TEMPLATE = "{Question}  Output the thinking process in <think> </think> and final answer (number) in <answer> </answer> tags."
+
+def main(script_args, training_args, model_args):    
+    model_id = "qwen/Qwen2.5-VL-3B-Instruct" 
+    dataset = load_dataset("leonardPKU/clevr_cogen_a_train")
+    total_samples = len(dataset["train"]) 
     train_dataset = []
-    for entry in original_train_dataset:
+    for idx, entry in enumerate(dataset["train"]):
+        if idx % 100 == 0 or idx == total_samples - 1: 
+            print(f"🔍 {idx} data have been loaded") 
+        if idx == 1000:
+            break
         new_entry = {
-            "prompt": entry["prompt"],
-            "image_path": entry["image_path"],
-            "conversations": [
+            "image": entry["image"],
+            "solution": entry["solution"],
+            "prompt": [
                 {
                     "role": "user",
                     "content": [
-                        {"type": "image", "url": entry["image_path"]},
-                        {"type": "text", "text": entry["prompt"]},
+                        {"type": "image", "url": entry["image"]},
+                        {"type": "text", "text": QUESTION_TEMPLATE.format(Question=entry["problem"])},
                     ]
                 }
             ]
         }
-
         train_dataset.append(new_entry)
 
-    trainer = MinimalGRPOTrainer(
-        model_name=model_id,
-        reward_func=format_reward,
-        args=config,
-        train_dataset=train_dataset,
-    )
-    trainer.train()
+    eval_dataset = []
+    with open("./prompts/superclevr_test200_counting_problems.jsonl", "r") as f:
+        for line in f:
+            entry = json.loads(line)
+            new_entry = {
+                "prompt": QUESTION_TEMPLATE.format(Question=entry["question"]),
+                "image": entry["image_path"],
+                "ground_truth": entry["ground_truth"]
+            }
+            eval_dataset.append(new_entry)
 
+    trainer = QwenGRPOTrainer(
+        model_name=model_id,
+        reward_funcs=[accuracy_reward, format_reward],
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset
+    )
+
+    trainer.train() 
+    trainer.evaluate(eval_dataset)  
+
+if __name__ == "__main__":
+    parser = TrlParser((GRPOScriptArguments, GRPOConfig, ModelConfig))
+    script_args, training_args, model_args = parser.parse_args_and_config()
+    main(script_args, training_args, model_args)
